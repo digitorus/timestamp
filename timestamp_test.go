0c40a443893680e18ee2e7ee1b3631879e205570
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2017-04-20 07:55:40 +0000

    Merged examples with test file

--- timestamp_test.go
+++ timestamp_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto"
 	"crypto/sha256"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -118,4 +119,31 @@ func TestCreateRequest(t *testing.T) {
 	if !bytes.Equal(reqCheck.HashedMessage, hashedMsg) {
 		t.Errorf("reqCheck.HashedMessage: got %x, want %x", reqCheck.HashedMessage, hashedMsg)
 	}
+}
+
+// ExampleCreateRequest demonstrates how to create a new time-stamping request
+// for an io.Reader.
+func ExampleCreateRequest() {
+	_, err := CreateRequest(strings.NewReader("Content to by time-stamped"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ExampleParseRequest demonstrates how to parse a raw der time-stamping request
+func ExampleParseRequest() {
+	// CreateRequest returns the request in der bytes
+	createdRequest, err := CreateRequest(strings.NewReader("Content to by time-stamped"), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// ParseRequest parses a request in der bytes
+	parsedRequest, err := ParseRequest(createdRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%x\n", parsedRequest.HashedMessage)
+	// Output: 62633c3232115454963ce641e5095c48a85dbec913a08332ad38586b910a3b27
 }
