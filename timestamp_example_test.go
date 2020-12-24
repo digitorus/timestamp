3bf8ebef56d51f8da428a5c14d4e3032749b8491
Paul van Brouwershaven <paul.vanbrouwershaven@digitorus.com>
2020-12-24 09:55:05 +0100

    Fix example output/test for Go 1.15
    `Name.String now prints non-standard attributes from Names if ExtraNames is nil.`
    https://golang.org/doc/go1.15#crypto/x509/pkix
    
    Fixes #8

--- timestamp_example_test.go
+++ timestamp_example_test.go
@@ -45,12 +45,12 @@ func ExampleCreateRequest_ParseResponse() {
 	fmt.Println(tsResp.HashedMessage)
 	fmt.Println(tsResp.Policy)
 	if len(tsResp.Certificates) > 0 {
-		fmt.Println(tsResp.Certificates[0].Subject)
+		fmt.Println(tsResp.Certificates[0].Subject.Organization)
 	}
 
 	// Output:
 	// [140 222 43 143 28 80 96 97 4 176 145 205 188 119 197 142 149 101 26 96 188 163 178 64 230 162 199 171 176 178 173 128]
 	// 1.2.3.4.1
-	// CN=www.freetsa.org,OU=TSA,O=Free TSA,L=Wuerzburg,ST=Bayern,C=DE
+	// [Free TSA]
 
 }
